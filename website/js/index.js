1a572c34ec939613ae2bd4a2f2f618b3f12dcba2
Ryan Dawson <rdawson40601@gmail.com>
2022-04-23 22:37:56 -0400

    Removed unused code

--- website/js/index.js
+++ website/js/index.js
@@ -1,9 +1,21 @@
-var images = ["../img/Toucan_Sam.png", "../img/Chef.jpg", "../img/Perry.png", "../img/fish.jpeg",
- "../img/buzz.png", "../img/campbell.jpeg", "../img/mason1.jpg", "../img/mason2.jpg", "../img/mason3.jpg",
-"../img/mario1.jpg", "../img/kyle1.jpg", "../img/zach1.jpg"];
+var images = [
+  "../img/Toucan_Sam.png",
+  "../img/Chef.jpg",
+  "../img/Perry.png",
+  "../img/fish.jpeg",
+  "../img/buzz.png",
+  "../img/campbell.jpeg",
+  "../img/mason1.jpg",
+  "../img/mason2.jpg",
+  "../img/mason3.jpg",
+  "../img/mario1.jpg",
+  "../img/kyle1.jpg",
+  "../img/zach1.jpg",
+  "../img/ringo.jpg",
+  "../img/akash.jpg",
+  "../img/ayman.jpg",
+];
 var currentImage = 0;
-
-
 var count1 = 0;
 var count2 = 0;
 var gameState = 0;
@@ -13,37 +25,35 @@ var gameState = 0;
 */
 
 function startGame() {
-    gameState = 1;
-    iterateImg();
-    document.getElementById("startBtn").style.display = "none";
-    document.getElementById("smashBtn").style.display = "block";
-    document.getElementById("passBtn").style.display = "block";
-    
+  gameState = 1;
+  iterateImg();
+  document.getElementById("startBtn").style.display = "none";
+  document.getElementById("smashBtn").style.display = "block";
+  document.getElementById("passBtn").style.display = "block";
 }
 
 function smashClick() {
-    if (gameState == 1) {
-        count1++;
-        document.getElementById("display1").innerHTML = count1;
-    }
+  if (gameState == 1) {
+    count1++;
+    document.getElementById("display1").innerHTML = count1;
+  }
 }
 
 function passClick() {
-    if (gameState == 1) {
-        count2++;
-        document.getElementById("display2").innerHTML = count2;
-    }
+  if (gameState == 1) {
+    count2++;
+    document.getElementById("display2").innerHTML = count2;
+  }
 }
+
 function iterateImg() {
-    if (gameState == 1) {
-        currentImage += 1;
+  if (gameState == 1) {
+    currentImage += 1;
 
-        if (currentImage >= images.length) {
-            currentImage = 0;
-        }
-
-        document.getElementById("mainImage").src = images[currentImage];
+    if (currentImage >= images.length) {
+      currentImage = 0;
     }
+
+    document.getElementById("mainImage").src = images[currentImage];
+  }
 }
-
-
